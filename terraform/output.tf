9e5549e0aed36383f136dd11374a7847be666e53
admin <admin@admins-MacBook-Pro.local>
2024-07-04 17:10:38 -0500

    terraform apply and destroy

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.dev-instance.id
+  value       = aws_instance.dev-instance02.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.dev-instance.public_ip
+  value       = aws_instance.dev-instance02.public_ip
 }
